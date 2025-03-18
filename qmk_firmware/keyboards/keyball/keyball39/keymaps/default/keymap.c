14dd805a6f6b716b6baca28d0dff8ff61ae0408f
dailyasian27 <steven.dinh27@gmail.com>
2025-03-18 07:05:52 -0700

    Update keymap.c Layer 1

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_F     , KC_P     , KC_B     ,                            KC_J     , KC_L     , KC_O     , KC_Y     , KC_:     ,
     KC_A     , KC_R     , KC_S     , KC_T     , KC_G     ,                            KC_M     , KC_N     , KC_E     , KC_I     , KC_U  ,
     KC_Z     , KC_X     , KC_C     , KC_C     , KC_V     ,                            KC_K     , KC_H     , KC_COMM  , KC_DOT   , KC_SLSH  ,
-    KC_ESC   , AML_TO   , KC_DEL  ,LSFT_T(KC_LNG2),LT(1,KC_SPC),LT(3,KC_LNG1),KC_BSPC,LT(2,KC_ENT),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, KC_RSFT
+    KC_ESC   , AML_TO   , KC_DEL  ,LT(1, KC_BSPC),LT(2,KC_ENTER),TG(4),KC_TAB,LT(2,KC_SPACE),LSFT_T(KC_LNG2),KC_RALT,KC_RGUI, SCRL_MO
   ),
 
   [1] = LAYOUT_universal(
